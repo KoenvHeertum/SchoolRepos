4f78a7e9c5053e50b19408230bed4de704ac6c5c
KoenvHeertum <koen.vanheertum@student.hu.nl>
2020-02-09 22:07:55 +0100

    Elif gefixed in fizzbuzz

--- Structured Programming/Formatieve opdracht 1/Opdracht 12.py
+++ Structured Programming/Formatieve opdracht 1/Opdracht 12.py
@@ -3,7 +3,7 @@ def fizzBuzz():
     for c in range(1, 101):
         if c % 3 == 0 and c % 5 == 0:
             print("FizzBuzz")
-        if c % 3 == 0:
+        elif c % 3 == 0:
             print("Fizz")
         elif c % 5 == 0:
             print("Buzz")
